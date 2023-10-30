2cc1ad6c83148aead5a7fa2b26508d5eadf3d860
bread sticks <uubixx.0208@gmail.com>
2023-10-30 20:31:59 +0000

    finished code

--- river-problem.py
+++ river-problem.py
@@ -2,16 +2,57 @@ import copy
 
 # Define a function that takes in a state as a dictionary and returns True if the state meets the conditions and False if it does not
 def isValid(state):
-    
+    if state["wolf"] == state["goat"] and state["wolf"] != state["person"]:
+        return False
+    elif state["wolf"] == state["cabbage"] and state["goat"] != state["person"]:
+        return False
+    else:
+        return True
 
 # Define a function that takes in a state as a dictionary and returns a list of all valid states that can be reached from 1 move of the input state
 # This function will need to call the function isValid(state)
 def get_next_states(state):
+    next_states = []
+    same_side = []
+    for thing in state :
+        if state[thing] == state["person"] and thing != "person":
+            same_side.append(thing)
+
+    for thing in same_side:
+        next_state = copy.deepcopy(state)
+        next_state[thing] = not state[thing]
+        next_state["person"] = not state["person"]
+
+        if isValid(next_state) == True:
+            next_states.append(next_state)
+
+    just_person = copy.deepcopy(state)
+    just_person["person"] = not state["person"]
+
+    if isValid(just_person) == True:
+        next_states.append(just_person)
+
+    return next_states
+
 
 
 # Define a recursive function that takes in a current_state and win_state and returns the path to those states using the Depth First Search algorithm
 # This function will need to call the function get_next_states(state), as well as itself
+
 def dfs(current_state, win_state):
+    if current_state == win_state:
+        return True
+
+    next_states = get_next_states(current_state)
+    visited_states.append(current_state)
+
+    for state in next_states:
+        if state not in visited_states:
+            path.append(state)
+            if dfs(state, win_state) == True:
+                return True
+            path.pop()
+
     
     
 # Test your code! Does it solve the river crossing riddle?
--- wikipedia-game-sc.py
+++ wikipedia-game-sc.py
@@ -24,12 +24,36 @@ def wikipedia_game_solver(start_page, target_page):
     visited = []
     queue = Queue()
     path = []
+    parent = {}
 
+
+    queue.put(start_page.title)
     visited.append(start_page.title)
+
     while not queue.empty():
-        
-    # queue.get()
-    # queue.put(page)
+        current_title = queue.get()
+        if current_title == target_page.title:
+            break
+
+        current_page = wiki_wiki.page(current_title)
+        next_level = fetch_links(current_page)
+        visited.append(current_title)
+
+        for node in next_level:
+            if node not in visited:
+                queue.put(node)
+                parent[node] = current_title
+
+    child = target_page.title
+
+    while child != start_page.title:
+        path.append(child)
+        child = parent[child]
+    path.append(start_page.title)
+    path.reverse()
+
+    
+
 
     end_time = time.time()
     print("This algorithm took", end_time-start_time, "seconds to run!")
@@ -38,7 +62,7 @@ def wikipedia_game_solver(start_page, target_page):
 
 # Example usage:
 start_page = wiki_wiki.page('Nina Tandon')
-target_page = wiki_wiki.page('Italian language')
+target_page = wiki_wiki.page('Italian Language')
 path = wikipedia_game_solver(start_page, target_page)
 print("Shortest path:", path)
 
